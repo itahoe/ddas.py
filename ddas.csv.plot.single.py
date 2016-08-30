55466c5adf5a9abaf8e77744cf06bf8b11c87e05
A-res <a.ress@live.ru>
2016-08-30 11:16:53 +0300

    Add files via upload

--- ddas.csv.plot.single.py
+++ ddas.csv.plot.single.py
@@ -12,8 +12,19 @@ k=root.fileName
 print(k)
 root.destroy()
 import pandas as pd
+#data = pd.read_csv('T0000ALL.csv',sep=u',', header=16, names=['TIME'],usecols=[0,1,2,3,4,5,6,7,8,9],skip_footer=0, index_col=0)
+#data = pd.read_csv('T0000ALL.csv',sep=u',', header=16, names=['TIME','CH1','CH1 Peak Detect','CH2','CH2 Peak Detect','CH3','CH3 Peak Detect','CH4','CH4 Peak Detect'],skip_footer=0, index_col=0)
+#data = pd.read_csv('T0000ALL.csv',sep=u',', header=14, skip_footer=0, index_col=0)
 data = pd.read_csv(k,sep=u',', header=14, skip_footer=0)
-data.plot(x='TIME',y=['CH1','CH2','CH3','CH4'],figsize=(14, 10))
-data.plot(x='TIME',y=['CH1 Peak Detect','CH2 Peak Detect','CH3 Peak Detect','CH4 Peak Detect'],figsize=(14, 10))
+print data
+l=len(data.columns)
+print l
+if l==3:
+    data.plot(x='TIME',y=['CH1'],figsize=(14, 10)
+    data.plot(x='TIME',y=['CH1 Peak Detect'],figsize=(14, 10))
+elif l==9:
+    data.plot(x='TIME',y=['CH1','CH2','CH3','CH4'],figsize=(14, 10))
+    data.plot(x='TIME',y=['CH1 Peak Detect','CH2 Peak Detect','CH3 Peak Detect','CH4 Peak Detect'],figsize=(14, 10))
+        
 
 pylab.show()
