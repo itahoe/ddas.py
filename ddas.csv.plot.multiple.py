f6d5fded844c4904b795c7593fef0772058302f0
A-res <a.ress@live.ru>
2016-09-08 15:30:00 +0300

    Update ddas.csv.plot.multiple.py

--- ddas.csv.plot.multiple.py
+++ ddas.csv.plot.multiple.py
@@ -9,168 +9,86 @@ filez = tkFileDialog.askopenfilenames(parent=root,title='Choose 2 or more files'
 t=list(filez)
 p=len(filez)
 root.destroy()
-print(p)
+
+if p==1:
+    data0 = pd.read_csv(t[0],sep=u',', header=14, skip_footer=0)
+    plt.plot(data0) 
 if p==2:
- data0 = pd.read_csv(t[0],sep=u',', header=14, skip_footer=0)
- df0 = pd.DataFrame(data=data0,columns=['CH1', 'CH2', 'CH3', 'CH4'])
+    data0 = pd.read_csv(t[0],sep=u',', header=14, skip_footer=0)
+    data1 = pd.read_csv(t[1],sep=u',', header=14, skip_footer=0)
+    plt.plot(data0) 
+    plt.plot(data1)
+if p==3:
+    data0 = pd.read_csv(t[0],sep=u',', header=14, skip_footer=0)
+    data1 = pd.read_csv(t[1],sep=u',', header=14, skip_footer=0)
+    data2 = pd.read_csv(t[2],sep=u',', header=14, skip_footer=0)
+    plt.plot(data0) 
+    plt.plot(data1) 
+    plt.plot(data2) 
+if p==4:
+    data0 = pd.read_csv(t[0],sep=u',', header=14, skip_footer=0)
+    data1 = pd.read_csv(t[1],sep=u',', header=14, skip_footer=0)
+    data2 = pd.read_csv(t[2],sep=u',', header=14, skip_footer=0)
+    data3 = pd.read_csv(t[3],sep=u',', header=14, skip_footer=0)
+    plt.plot(data0) 
+    plt.plot(data1) 
+    plt.plot(data2) 
+    plt.plot(data3)
+if p==5:
+    data0 = pd.read_csv(t[0],sep=u',', header=14, skip_footer=0)
+    data1 = pd.read_csv(t[1],sep=u',', header=14, skip_footer=0)
+    data2 = pd.read_csv(t[2],sep=u',', header=14, skip_footer=0)
+    data3 = pd.read_csv(t[3],sep=u',', header=14, skip_footer=0)
+    data4 = pd.read_csv(t[4],sep=u',', header=14, skip_footer=0)
+    plt.plot(data0) 
+    plt.plot(data1) 
+    plt.plot(data2) 
+    plt.plot(data3) 
+    plt.plot(data4) 
+if p==6:
+    data0 = pd.read_csv(t[0],sep=u',', header=14, skip_footer=0)
+    data1 = pd.read_csv(t[1],sep=u',', header=14, skip_footer=0)
+    data2 = pd.read_csv(t[2],sep=u',', header=14, skip_footer=0)
+    data3 = pd.read_csv(t[3],sep=u',', header=14, skip_footer=0)
+    data4 = pd.read_csv(t[4],sep=u',', header=14, skip_footer=0)
+    data5 = pd.read_csv(t[5],sep=u',', header=14, skip_footer=0)
+    plt.plot(data0) 
+    plt.plot(data1) 
+    plt.plot(data2) 
+    plt.plot(data3) 
+    plt.plot(data4) 
+    plt.plot(data5)
+if p==7:
+    data0 = pd.read_csv(t[0],sep=u',', header=14, skip_footer=0)
+    data1 = pd.read_csv(t[1],sep=u',', header=14, skip_footer=0)
+    data2 = pd.read_csv(t[2],sep=u',', header=14, skip_footer=0)
+    data3 = pd.read_csv(t[3],sep=u',', header=14, skip_footer=0)
+    data4 = pd.read_csv(t[4],sep=u',', header=14, skip_footer=0)
+    data5 = pd.read_csv(t[5],sep=u',', header=14, skip_footer=0)
+    data6 = pd.read_csv(t[6],sep=u',', header=14, skip_footer=0)
+    plt.plot(data0) 
+    plt.plot(data1) 
+    plt.plot(data2) 
+    plt.plot(data3) 
+    plt.plot(data4) 
+    plt.plot(data5)
+    plt.plot(data6)
+if p==8:
+    data0 = pd.read_csv(t[0],sep=u',', header=14, skip_footer=0)
+    data1 = pd.read_csv(t[1],sep=u',', header=14, skip_footer=0)
+    data2 = pd.read_csv(t[2],sep=u',', header=14, skip_footer=0)
+    data3 = pd.read_csv(t[3],sep=u',', header=14, skip_footer=0)
+    data4 = pd.read_csv(t[4],sep=u',', header=14, skip_footer=0)
+    data5 = pd.read_csv(t[5],sep=u',', header=14, skip_footer=0)
+    data6 = pd.read_csv(t[6],sep=u',', header=14, skip_footer=0)
+    data7 = pd.read_csv(t[7],sep=u',', header=14, skip_footer=0)   
+    plt.plot(data0) 
+    plt.plot(data1) 
+    plt.plot(data2) 
+    plt.plot(data3) 
+    plt.plot(data4) 
+    plt.plot(data5)
+    plt.plot(data6)
+    plt.plot(data7)
 
- data1 = pd.read_csv(t[1],sep=u',', header=14, skip_footer=0)
- df1 = pd.DataFrame(data=data1, columns=['CH1', 'CH2', 'CH3', 'CH4'])  
-
- f, ax = plt.subplots(2,2,figsize=(20, 10))
- ax[0,0].plot(df0)
- ax.set_title("Title for second plot")
- ax[0,1].plot(df1)
-##################
-elif p==3:
- data0 = pd.read_csv(t[0],sep=u',', header=14, skip_footer=0)
- df0 = pd.DataFrame(data=data0,columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- data1 = pd.read_csv(t[1],sep=u',', header=14, skip_footer=0)
- df1 = pd.DataFrame(data=data1, columns=['CH1', 'CH2', 'CH3', 'CH4'])  
-
- data2 = pd.read_csv(t[2],sep=u',', header=14, skip_footer=0)
- df2 = pd.DataFrame(data=data2, columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- f, ax = plt.subplots(2,2,figsize=(20, 10))
- ax[0,0].plot(df0)
- ax[0,1].plot(df1)
- ax[1,0].plot(df2)
-#################
-elif p==4:
- data0 = pd.read_csv(t[0],sep=u',', header=14, skip_footer=0)
- df0 = pd.DataFrame(data=data0,columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- data1 = pd.read_csv(t[1],sep=u',', header=14, skip_footer=0)
- df1 = pd.DataFrame(data=data1, columns=['CH1', 'CH2', 'CH3', 'CH4'])  
-
- data2 = pd.read_csv(t[2],sep=u',', header=14, skip_footer=0)
- df2 = pd.DataFrame(data=data2, columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- data3 = pd.read_csv(t[3],sep=u',', header=14, skip_footer=0)
- df3 = pd.DataFrame(data=data3, columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- f, ax = plt.subplots(2,2,figsize=(20, 10))
- ax[0,0].plot(df0)
- ax[0,1].plot(df1)
- ax[1,0].plot(df2)
- ax[1,1].plot(df3)
-##################### 
-elif p==5:
- data0 = pd.read_csv(t[0],sep=u',', header=14, skip_footer=0)
- df0 = pd.DataFrame(data=data0,columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- data1 = pd.read_csv(t[1],sep=u',', header=14, skip_footer=0)
- df1 = pd.DataFrame(data=data1, columns=['CH1', 'CH2', 'CH3', 'CH4'])  
-
- data2 = pd.read_csv(t[2],sep=u',', header=14, skip_footer=0)
- df2 = pd.DataFrame(data=data2, columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- data3 = pd.read_csv(t[3],sep=u',', header=14, skip_footer=0)
- df3 = pd.DataFrame(data=data3, columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- data4 = pd.read_csv(t[4],sep=u',', header=14, skip_footer=0)
- df4 = pd.DataFrame(data=data3, columns=['CH1', 'CH2', 'CH3', 'CH4'])
- 
- f, ax = plt.subplots(3,2,figsize=(20, 10))
- ax[0,0].plot(df0)
- ax[0,1].plot(df1)
- ax[1,0].plot(df2)
- ax[1,1].plot(df3)
- ax[2,1].plot(df3)
- #####################
-elif p==6:
- data0 = pd.read_csv(t[0],sep=u',', header=14, skip_footer=0)
- df0 = pd.DataFrame(data=data0,columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- data1 = pd.read_csv(t[1],sep=u',', header=14, skip_footer=0)
- df1 = pd.DataFrame(data=data1, columns=['CH1', 'CH2', 'CH3', 'CH4'])  
-
- data2 = pd.read_csv(t[2],sep=u',', header=14, skip_footer=0)
- df2 = pd.DataFrame(data=data2, columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- data3 = pd.read_csv(t[3],sep=u',', header=14, skip_footer=0)
- df3 = pd.DataFrame(data=data3, columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- data4 = pd.read_csv(t[4],sep=u',', header=14, skip_footer=0)
- df4 = pd.DataFrame(data=data4, columns=['CH1', 'CH2', 'CH3', 'CH4'])
- 
- data5 = pd.read_csv(t[5],sep=u',', header=14, skip_footer=0)
- df5 = pd.DataFrame(data=data5, columns=['CH1', 'CH2', 'CH3', 'CH4'])
- 
- f, ax = plt.subplots(3,2,figsize=(20, 10))
- ax[0,0].plot(df0)
- ax[0,1].plot(df1)
- ax[1,0].plot(df2)
- ax[1,1].plot(df3)
- ax[2,1].plot(df4)
- ax[2,2].plot(df5)
- #####################
-elif p==7:
- data0 = pd.read_csv(t[0],sep=u',', header=14, skip_footer=0)
- df0 = pd.DataFrame(data=data0,columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- data1 = pd.read_csv(t[1],sep=u',', header=14, skip_footer=0)
- df1 = pd.DataFrame(data=data1, columns=['CH1', 'CH2', 'CH3', 'CH4'])  
-
- data2 = pd.read_csv(t[2],sep=u',', header=14, skip_footer=0)
- df2 = pd.DataFrame(data=data2, columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- data3 = pd.read_csv(t[3],sep=u',', header=14, skip_footer=0)
- df3 = pd.DataFrame(data=data3, columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- data4 = pd.read_csv(t[4],sep=u',', header=14, skip_footer=0)
- df4 = pd.DataFrame(data=data4, columns=['CH1', 'CH2', 'CH3', 'CH4'])
- 
- data5 = pd.read_csv(t[5],sep=u',', header=14, skip_footer=0)
- df5 = pd.DataFrame(data=data5, columns=['CH1', 'CH2', 'CH3', 'CH4'])
- 
- data6 = pd.read_csv(t[6],sep=u',', header=14, skip_footer=0)
- df6 = pd.DataFrame(data=data6, columns=['CH1', 'CH2', 'CH3', 'CH4'])
- 
- f, ax = plt.subplots(4,2,figsize=(20, 10))
- ax[0,0].plot(df0)
- ax[0,1].plot(df1)
- ax[1,0].plot(df2)
- ax[1,1].plot(df3)
- ax[2,1].plot(df4)
- ax[2,2].plot(df5)
- ax[3,1].plot(df6)
- #####################
-elif p==8:
- data0 = pd.read_csv(t[0],sep=u',', header=14, skip_footer=0)
- df0 = pd.DataFrame(data=data0,columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- data1 = pd.read_csv(t[1],sep=u',', header=14, skip_footer=0)
- df1 = pd.DataFrame(data=data1, columns=['CH1', 'CH2', 'CH3', 'CH4'])  
-
- data2 = pd.read_csv(t[2],sep=u',', header=14, skip_footer=0)
- df2 = pd.DataFrame(data=data2, columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- data3 = pd.read_csv(t[3],sep=u',', header=14, skip_footer=0)
- df3 = pd.DataFrame(data=data3, columns=['CH1', 'CH2', 'CH3', 'CH4'])
-
- data4 = pd.read_csv(t[4],sep=u',', header=14, skip_footer=0)
- df4 = pd.DataFrame(data=data4, columns=['CH1', 'CH2', 'CH3', 'CH4'])
- 
- data5 = pd.read_csv(t[5],sep=u',', header=14, skip_footer=0)
- df5 = pd.DataFrame(data=data5, columns=['CH1', 'CH2', 'CH3', 'CH4'])
- 
- data6 = pd.read_csv(t[6],sep=u',', header=14, skip_footer=0)
- df6 = pd.DataFrame(data=data6, columns=['CH1', 'CH2', 'CH3', 'CH4'])
- 
- data7 = pd.read_csv(t[7],sep=u',', header=14, skip_footer=0)
- df7 = pd.DataFrame(data=data7, columns=['CH1', 'CH2', 'CH3', 'CH4'])
- 
- f, ax = plt.subplots(4,2,figsize=(20, 10))
- ax[0,0].plot(df0)
- ax[0,1].plot(df1)
- ax[1,0].plot(df2)
- ax[1,1].plot(df3)
- ax[2,0].plot(df4)
- ax[2,1].plot(df5)
- ax[3,0].plot(df6)
- ax[3,1].plot(df7)
- #####################
 plt.show()
-
