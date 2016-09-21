df8e56f0e232a4cc2471b5894d98f873efdec233
A-res <a.ress@live.ru>
2016-09-21 10:15:27 +0300

    Update ddas.csv.pcb.plot.single.py

--- ddas.csv.pcb.plot.single.py
+++ ddas.csv.pcb.plot.single.py
@@ -1,5 +1,17 @@
-import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import pylab
+import pandas as pd
+import Tkinter,tkFileDialog
+
+root = Tkinter.Tk() 
+
+
+root.fileName=tkFileDialog.askopenfilename(filetypes=(("csv files","*csv"),("All files","*.*" )))
+fn=root.fileName
+#df = pd.read_csv(fn, skipinitialspace=True)
+root.destroy()
+#plot_df(df, out_plot_fn=r'd:/bad.png')
 
 def plot_df(df, out_plot_fn=None, mult_factor=0.8,
             cols=None, x_axis=None, figsize=(12,10)):
@@ -19,14 +31,6 @@ def plot_df(df, out_plot_fn=None, mult_factor=0.8,
     else:
         plt.show()
 
-
-fn = r'D:\temp\.data\7.csv.gz'
-
-df = pd.read_csv(fn, skipinitialspace=True)
-
-plot_df(df, out_plot_fn=r'd:/temp/bad.png')
-
-
 # пропускаем 7-ю колонку (Python/Pandas считают с нуля, поэтому - 6-ю)
 # иначе по Y-axis будет неправильное масштабирование
 # и все графики будут выглядеть слишком сглаженными 
@@ -34,11 +38,14 @@ usecols = [0,1,2,3,4,5,7]
 
 df = pd.read_csv(fn, usecols=usecols, skipinitialspace=True)
 
-plot_df(df, out_plot_fn=r'd:/temp/a.png')
+#plot_df(df, out_plot_fn=r'd:/a.png')
 
-# переименуем названия колонок в DF
+# переименуем названия датчиков
 cols = ['ch1','ch2','ch3','ch4','ch5','ch6','ch8']
-plot_df(df, cols=cols, out_plot_fn=r'd:/temp/b.png')
+plot_df(df, cols=cols, out_plot_fn=r'd:/b.png',figsize=(17, 14))
+plt.show()
 
 # только первые 50 строк
-plot_df(df.head(50), out_plot_fn=r'd:/temp/c.png')
+#plot_df(df.head(50), out_plot_fn=r'd:/c.png')
+plt.show()
+input()
